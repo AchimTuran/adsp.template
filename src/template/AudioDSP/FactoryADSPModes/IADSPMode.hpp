9704201fc486495bbdfeb1993712e21045fa4be9
Achim Turan <mail@achim-turan.de>
2016-03-06 00:32:55 +0100

    [FactoryADSPModes][IADSPModes] Add basic processing mode support

--- src/template/AudioDSP/FactoryADSPModes/IADSPMode.hpp
+++ src/template/AudioDSP/FactoryADSPModes/IADSPMode.hpp
@@ -26,19 +26,113 @@
 class IADSPMode
 {
 public:
-  // Create/Destroy Methods
-  virtual AE_DSP_ERROR Create() = 0;
+  // Requiered Create/Destroy Methods
+  virtual AE_DSP_ERROR Create(const AE_DSP_SETTINGS *settings, const AE_DSP_STREAM_PROPERTIES *pProperties) = 0;
   virtual AE_DSP_ERROR Destroy() = 0;
 
   // Requiered Processing Methods
   virtual unsigned int Process(float **Array_in, float **Array_out, unsigned int Samples) = 0;
-  virtual unsigned int NeededSamplesize();
-  virtual float GetDelay();
+  
 
-  // Optional Methods for Master Modes
-  virtual int MasterProcessGetOutChannels(unsigned long &OutChannelFlags);
-  virtual const char *MasterProcessGetStreamInfoString();
+// Optional common Methods
+  virtual unsigned int NeededSamplesize()
+  {
+    return 1024;
+  }
 
-  // Optional Resampling Methods
-  virtual int ResamplingRate();
+  virtual float GetDelay()
+  {
+    return (float)m_StreamSettings.iInFrames / (float)m_StreamSettings.iInSamplerate;
+  }
+
+  //! This method checks the input stream and can filter processing mode types.
+  /*!
+  * Ask the add-on about a requested processing mode that it is supported on the current
+  * stream. Is called about every add-on mode after successed StreamCreate.
+  * @param Type The processing mode type, see AE_DSP_MODE_TYPE for definitions
+  * @param Mode_id The mode inside add-on which must be performed on call.
+  * Id is set from add-on and can be modified templateConfiguration.cpp by
+  * editing adspPreModeNum array. Best practise is to use a global enum to
+  * have only a mode id once. For example see adspProcessingModeIDs in
+  * templateConfiguration.h
+  * @param Unique_db_mode_id The Mode unique id generated from dsp database.
+  * @return AE_DSP_ERROR_NO_ERROR if the properties were fetched successfully.
+  * If the stream is not supported the ADSP addon must return AE_DSP_ERROR_IGNORE_ME.
+  * @remarks By default this method accept all processing types.
+  * If you wanna filter processing mode types then you have to overload this method in
+  * your processing class.
+  */
+  virtual AE_DSP_ERROR StreamIsModeSupported(AE_DSP_MODE_TYPE Type, unsigned int Mode_id, int Unique_db_mode_id)
+  {
+    return AE_DSP_ERROR_NO_ERROR;
+  }
+
+  //!  This gets the current stream settings and properties. 
+  /*!
+  * Get stream settings and properties. For details see  and AE_DSP_STREAM_PROPERTIES structures.
+  * If the add-on operate with buffered arrays and the output size can be higher as
+  * the input it becomes asked about needed size before any PostProcess call.
+  * @param pSettings Stream settings for details see AE_DSP_SETTINGS.
+  * @param pProperties Stream properties for details see AE_DSP_STREAM_PROPERTIES.
+  * @return AE_DSP_ERROR_INVALID_PARAMETERS: if your input parameters were invalid.
+  * AE_DSP_ERROR_NO_ERROR: if all was ok.
+  */
+  AE_DSP_ERROR GetStreamInfos(const AE_DSP_SETTINGS *pSettings, const AE_DSP_STREAM_PROPERTIES* pProperties, void *CustomStreamInfos = NULL)
+  {
+    if (!pSettings || !pProperties)
+    {
+      return AE_DSP_ERROR_INVALID_PARAMETERS;
+    }
+
+    memcpy((void*)pSettings, &m_StreamSettings, sizeof(AE_DSP_SETTINGS));
+    memcpy((void*)pProperties, &m_StreamProperties, sizeof(AE_DSP_STREAM_PROPERTIES));
+
+    if (CustomStreamInfos)
+    {
+      return GetCustomStreamInfos(CustomStreamInfos);
+    }
+
+    return AE_DSP_ERROR_NO_ERROR;
+  }
+
+// Optional Methods for Master Modes
+  virtual int MasterProcessGetOutChannels(unsigned long &OutChannelFlags)
+  {
+    return -1;
+  }
+
+  //!  Returns an information string, which will be shown in the UI.
+  /*!
+   * @return A string to show. This method always returns a empty string.
+   * @remarks If you wanna return a custom string, then you must overload this method
+   * in your processing class.
+   */
+  virtual const char *MasterProcessGetStreamInfoString()
+  {
+    return "";
+  }
+
+
+// Optional Resampling Methods
+  virtual int ResamplingRate()
+  {
+    return m_StreamSettings.iProcessSamplerate;
+  }
+
+protected:
+  //! ToDo: description.
+  /*!
+  * Returns ToDo!
+  * @return ToDo!
+  * @remarks ToDo!
+  */
+  virtual AE_DSP_ERROR GetCustomStreamInfos(void *CustomStreamSettings)
+  {
+    return AE_DSP_ERROR_NO_ERROR;
+  }
+
+  //! Used stream settings for details see AE_DSP_SETTINGS.
+  AE_DSP_SETTINGS m_StreamSettings;
+  //! Used stream properties for details see AE_DSP_STREAM_PROPERTIES.
+  AE_DSP_STREAM_PROPERTIES m_StreamProperties;
 };
